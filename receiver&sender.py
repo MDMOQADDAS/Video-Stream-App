858c565249b1bcfe266f76c1c0863be6f2d4b9af
MDMOQADDAS <69861558+MDMOQADDAS@users.noreply.github.com>
2021-06-12 15:26:19 +0530

    Add files via upload

---
+++ receiver&sender.py
@@ -0,0 +1,41 @@
+import socket
+import cv2
+import numpy as np
+
+# Server socket
+PORT = 1234
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind(('',PORT))
+
+s.listen(10)
+
+conn, addr = s.accept()
+print("Sreaming started....")
+# Client Socket
+capture = cv2.VideoCapture(1)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(('127.0.0.1', 50500))
+
+
+
+while True:
+    # Data Receive From Other Laptop
+    Video_Receive = conn.recv(90456) 
+    # Now doing some manupulation
+    Video_Receive_in_array = np.fromstring(Video_Receive, np.uint8)
+    #Now doing decoding
+    Other_laptop_video = cv2.imdecode(Video_Receive_in_array, cv2.IMREAD_COLOR)
+    # Showing the video which is come from the other laptop
+    cv2.imshow('Linux', Other_laptop_video)
+    if cv2.waitKey(10) == 13:
+        break
+    # Now caputuing own video 
+    ret, own_video = capture.read()
+    #Doing decoding
+    own_video_decode = cv2.imencode('.jpg', own_video)[1].tostring()
+    #Now sending own video|
+    sock.sendall(own_video_decode)
+
+cv2.destroyAllWindows()
+capture.release()
